bc2409db75b2d26826a630309870af1927c70c4f
Rocio <rolinoe@gmail.com>
2023-12-19 01:26:24 -0300

    nieve final

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const balls = document.querySelectorAll('.ball');
 const card = document.querySelector('.card');
 const cardTitle = document.getElementById('card-title');
 const cardText = document.getElementById('card-text');
+const container = document.querySelector('.snow-container');
+
+//Animacion nieve
+for (let i = 0; i < 100; i++) {
+  let leftSnow = Math.floor(Math.random() * container.clientWidth);
+  let topSnow = Math.floor(Math.random() * container.clientHeight);
+  let widthSnow = Math.floor(Math.random() * 50);
+  let timeSnow = Math.floor((Math.random() * 5) + 5);
+  let blurSnow = Math.floor(Math.random() * 10);
+
+  let div = document.createElement('div');
+  div.classList.add('snow');
+  div.style.left = leftSnow + 'px';
+  div.style.top = topSnow + 'px';
+  div.style.width = widthSnow + 'px';
+  div.style.height = widthSnow + 'px';
+  div.style.animationDuration = timeSnow + 's';
+  div.style.filter = 'blur(' + blurSnow + 'px)';
+  container.appendChild(div);
+}
 
 // Array de contenidos para la tarjeta
 const cardContents = [
